56fc9cefe22ff9673fda8eef8df039da12f16c72
GUSTAVO PIZATTO <gustavopizatto@hotmail.com>
2025-06-18 05:41:47 -0300

    Update Bobina.js

--- models/Bobina.js
+++ models/Bobina.js
@@ -1,23 +1,20 @@
 const mongoose = require('mongoose');
 
 const BobinaSchema = new mongoose.Schema({
-  tipoPapel: String,
-  fabricante: String,
-  peso: Number,
-  largura: Number,
-  gramatura: Number,
-  fornecedor: String,
-  localizacao: String,
-  status: {
-    type: String,
-    enum: ['DISPONIVEL', 'EM_USO'],
-    default: 'DISPONIVEL'
-  },
-  dataEntrada: {
-    type: Date,
-    default: Date.now
-  },
-  dataSaida: Date
+    tipoPapel: String,
+    fabricante: String,
+    peso: Number,
+    pesoInicial: Number,
+    largura: Number,
+    gramatura: Number,
+    fornecedor: String,
+    localizacao: String,
+    dataEntrada: Date,
+    dataSaida: Date,
+    codigoQR: {
+        type: String,
+        default: () => `BOBINA-${Date.now()}`
+    }
 });
 
 module.exports = mongoose.model('Bobina', BobinaSchema);
